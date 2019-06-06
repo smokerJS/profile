e51b2214508a4d6fa3649fe0017efdf322caf3f5
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-06-06 12:55:09 +0900

    layout default style setting

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
-
-        <nuxt />
+  <section class="layout-frame">
+    <nuxt />
+  </section>
 </template>
 
 <script>
@@ -11,3 +12,27 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+  * {
+    margin: 0 auto;
+    padding: 0;
+    list-style: none;
+  }
+  a {
+    text-decoration: none;
+  }
+  *:focus {
+    outline: none;
+  }
+
+  main {
+    position: relative;
+  }
+  .layout-frame {
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+  }
+</style>
