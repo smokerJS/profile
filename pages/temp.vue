ede6a4b9b6d248e23c57a519b9649ce3a63ee89d
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-07-12 13:27:37 +0900

    temp page update

--- pages/temp.vue
+++ pages/temp.vue
@@ -1,33 +1,22 @@
 <template>
-  <section class="index-page" @mousemove="mouseMoveHandler">
-    <IndexTitle ref="IndexTitleComponent"/>
-    <IndexBackground/>
+  <section class="popo">
+    test
   </section>
 </template>
 
 <script>
-import IndexTitle from '../components/IndexTitle';
-import IndexBackground from '../components/IndexBackground';
-
 
 export default {
   name: 'index',
   components: {
-    IndexBackground,
-    IndexTitle ,
+
   },
   methods: {
-    mouseMoveHandler(e) {
-      this.$refs.IndexTitleComponent.mouseMoveHandler(e);
-    }
+
   },
 }
 </script>
 
 <style lang="scss" scope>
-  .index-page {
-    width: 100%;
-    height: 100vh;
-    position: relative;
-  }
+
 </style>
