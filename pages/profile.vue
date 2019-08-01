075fc6f2bd5d89795f09bec312b08eeac2ff4744
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-08-01 16:45:55 +0900

    profile page create

---
+++ pages/profile.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    SmokerJS
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'profile',
+  components: {
+
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style lang="scss" scope>
+
+</style>
