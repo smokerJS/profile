b3661da2b996ce939876baa71c1f07400184111d
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-08-07 10:13:32 +0900

    tutor page copy

---
+++ pages/tutor.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <h1>TUTOR</h1>
+    <!-- <img/> -->'
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'tutor',
+  components: {
+
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style lang="scss" scope>
+
+</style>
