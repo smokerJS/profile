c6fc6d339bfc3ce67b399b8435247886fade6fed
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-09-05 19:28:50 +0900

    profile page update

--- pages/profile.vue
+++ pages/profile.vue
@@ -6,9 +6,9 @@
       <li>FrontEnd Devloper<li>
       <li>Jung, Jinsol</li>
       <li>smokerjs.dev@gmail.com</li>
-      <li>React Redux Next Hooks</li>
-      <li>VUE Nuxt</li>
-      <li>Webpack4 Sass</li>
+      <li>React, Redux, Next, Hooks</li>
+      <li>Vue, Nuxt</li>
+      <li>Webpak4, HTML5, Sass, Jquery</li>
     </ul>
   </section>
 </template>
