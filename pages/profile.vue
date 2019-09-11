1a13593a6a5cbf0691ce9725c9d0379efc594b96
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-09-11 13:33:16 +0900

    profile mail form

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,15 +1,27 @@
 <template>
   <section class="profile-page">
-    <img src="../assets/images/profile/img_profile.jpeg"/>
-    <h1>SmokerJS</h1>
-    <ul>
-      <li>FrontEnd Devloper<li>
-      <li>Jung, Jinsol</li>
-      <li>smokerjs.dev@gmail.com</li>
-      <li>React, Redux, Next, Hooks</li>
-      <li>Vue, Nuxt</li>
-      <li>Webpak4, HTML5, Sass, Jquery</li>
-    </ul>
+    <article>
+      <img src="../assets/images/profile/img_profile.jpeg"/>
+      <h1>SmokerJS</h1>
+      <ul>
+        <li>FrontEnd Devloper<li>
+        <li>Jung, Jinsol</li>
+        <li>smokerjs.dev@gmail.com</li>
+        <li>React, Redux, Next, Hooks</li>
+        <li>Vue, Nuxt</li>
+        <li>Webpak4, HTML5, Sass, Jquery</li>
+      </ul>
+    </article>
+    <article>
+      <h1>Mail</h1>
+      <div>
+        <strong>send user info</strong>
+      </div>
+      <div contenteditable="true">
+
+      </div>
+      <button>send</button>
+    </article>
   </section>
 </template>
 
