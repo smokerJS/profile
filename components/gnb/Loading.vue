2d34b7313c9bab4c314c414dd39db1cb5a1c1025
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-09-12 12:34:03 +0900

    GNB loading background color update

--- components/gnb/Loading.vue
+++ components/gnb/Loading.vue
@@ -21,7 +21,7 @@ export default {
     width: 100%;
     height: 100vh;
     z-index: 50;
-    background: rgba(255,0,0,.8);
+    background: rgba(70, 69, 102, 0.8);
     & div {
       float: left;
       $dotSize: 10;
