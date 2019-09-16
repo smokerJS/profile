53a4e355fb68ead7b089f7a5ee832392560c28bb
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-09-16 15:14:10 +0900

    default global scope scss delete

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,37 +18,6 @@ export default {
   },
 }
 </script>
-
-<style lang="scss">
-  $images: "//assets/images";
-  * {
-    margin: 0 auto;
-    padding: 0;
-    list-style: none;
-  }
-  a {
-    text-decoration: none;
-  }
-  *:focus {
-    outline: none;
-  }
-  .layout-frame {
-    position: relative;
-    width: 100%;
-    height: 100vh;
-    overflow: hidden;
-  }
-  @font-face {
-    font-family: "FightThis";
-    src: url("../assets/fonts/FightThis.eot");
-    src: local(※), url("../assets/fonts/FightThis.woff") format("woff");
-  }
-  @font-face {
-    font-family: "D2Coding";
-    src: url("../assets/fonts/D2Coding.eot");
-    src: local(※), url("../assets/fonts/D2Coding.woff") format("woff");
-  }
-</style>
 <style lang="scss" scope>
   .layout-frame {
     .license {
