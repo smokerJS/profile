a687506fb34a346331ba6a30ff56acc9aa9562f4
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-09-17 10:04:55 +0900

    profile page skillset delete

--- pages/profile.vue
+++ pages/profile.vue
@@ -3,14 +3,6 @@
     <article>
       <img :src="require('@images/profile/img_profile.jpeg')"/>
       <h1>SmokerJS</h1>
-      <ul>
-        <li>FrontEnd Devloper<li>
-        <li>Jung, Jinsol</li>
-        <li>smokerjs.dev@gmail.com</li>
-        <li>React, Redux, Next, Hooks</li>
-        <li>Vue, Nuxt</li>
-        <li>Webpak4, HTML5, Sass, Jquery</li>
-      </ul>
     </article>
     <article>
       <h1>Mail</h1>
