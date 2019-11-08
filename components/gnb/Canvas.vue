de40acffd686033fb637e660da1960b25d031354
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-11-08 14:56:41 +0900

    code refactoring

--- components/gnb/Canvas.vue
+++ components/gnb/Canvas.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="gnbCanvasArea">
     <GnbLoading v-if="this.load"/>
-    <canvas id="viewCanvasBack" ref="viewCanvasBack"></canvas>
-    <!-- <canvas id="viewCanvasCover" ref="viewCanvasCover"></canvas> -->
+    <canvas id="viewCanvas" ref="viewCanvas"></canvas>
   </div>
 </template>
 
@@ -23,6 +22,8 @@ export default {
       ctxBack : null,
       ctxCover : null,
       subCanvas : null,
+      innerWidth: 0,
+      innerHeight: 0,
       viewHeight : 0,
       viewWidth : 0,
       suvHeight : 0,
@@ -35,54 +36,13 @@ export default {
       return Math.floor(Math.random() * (max - min + 1)) + min;
     },
     loopBack() {
-      const verticalSlices = this.verticalSlices;
+      const { verticalSlices, subCanvas, ctx, innerWidth, innerHeight, viewHeight, viewWidth, subHeight, subWidth } = this;
 
-      const innerWidth = this.$refs.viewCanvasBack.width;
-      const innerHeight = this.$refs.viewCanvasBack.height;
-      this.ctxBack.fillStyle = "#1a191c";
-      this.ctxBack.fillRect(0, 0, innerWidth, innerHeight);
+      ctx.fillStyle = "#1a191c";
+      ctx.fillRect(0, 0, innerWidth, innerHeight);
 
-      this.ctxBack.shadowBlur = 0;
-      this.ctxBack.shadowColor = "none";
-            const colors = [
-        "#b4b2b5",
-        "#dfd73f",
-        "#6ed2dc",
-        "#66cf5d",
-        "#c542cb",
-        "#d0535e",
-        "#3733c9"
-      ];
-
-            for (let i = 0; i < 100; i+= 0.5)  {
-
-        let horizOffset = this.getRandom(-Math.abs(20), 20);
-        this.ctxBack.drawImage(
-          this.subCanvas,
-          0,
-          i * this.subHeight + 20,
-          this.subCanvas.width,
-          i * this.subHeight + this.subHeight,
-          horizOffset,
-          i * this.viewHeight,
-          this.viewWidth,
-          i * this.viewHeight + this.viewHeight
-        )
-      }
-
-for (let i = 0; i < 3; i+= 1)  {
-                    this.ctxBack.fillStyle = colors[Math.floor(Math.random() * 40)];
-      this.ctxBack.fillRect(
-        Math.random() * innerWidth - 20,
-        Math.random() * innerHeight - 20,
-        Math.random() * innerWidth - 20,
-        Math.random() * innerHeight - 20
-      );
-}
-      // this.ctxBack.setTransform(1, 0, 0, .8, .2, 0);
-      requestAnimationFrame(this.loopBack);
-    },
-    loopCover() {
+      ctx.shadowBlur = 0;
+      ctx.shadowColor = "none";
       const colors = [
         "#b4b2b5",
         "#dfd73f",
@@ -93,56 +53,45 @@ for (let i = 0; i < 3; i+= 1)  {
         "#3733c9"
       ];
 
-      const innerWidth = this.$refs.viewCanvasCover.width;
-      const innerHeight = this.$refs.viewCanvasCover.height;
-      this.ctxCover.fillStyle = "#1a191c";
-      this.ctxCover.fillRect(0, 0, innerWidth, innerHeight);
+      for (let i = 0; i < 100; i+= 0.5)  {
+        let horizOffset = this.getRandom(-Math.abs(20), 20);
+        ctx.drawImage(
+          subCanvas,
+          0,
+          i * subHeight + 20,
+          subCanvas.width,
+          i * subHeight + subHeight,
+          horizOffset,
+          i * viewHeight,
+          viewWidth,
+          i * viewHeight + viewHeight
+        )
+      }
 
-      this.ctxCover.shadowBlur = 0;
-      this.ctxCover.shadowColor = "none";
-      this.ctxCover.setTransform(1, 0, 0, 1, 0, 0);
-
-      for (let i = 0; i < 1000; i++) {
-        this.ctxCover.fillStyle = `rgba(255, 255, 255, ${Math.random() * 0.01})`;
-        this.ctxCover.fillRect(
-          Math.floor(Math.random() * innerWidth),
-          Math.floor(Math.random() * innerHeight),
-          Math.floor(Math.random() * 30) + 1,
-          Math.floor(Math.random() * 30) + 1
-        );
-
-        this.ctxCover.fillStyle = `rgba(0,0,0,${Math.random() * 0.1})`;
-        this.ctxCover.fillRect(
-          Math.floor(Math.random() * innerWidth),
-          Math.floor(Math.random() * innerHeight),
-          Math.floor(Math.random() * 25) + 1,
-          Math.floor(Math.random() * 25) + 1
+      for (let i = 0; i < 2; i+= 1)  {
+        ctx.fillStyle = colors[Math.floor(Math.random() * 40)];
+        ctx.fillRect(
+          Math.random() * innerWidth - 100,
+          Math.random() * innerHeight - 20,
+          Math.random() * innerWidth - 20,
+          Math.random() * innerHeight - 20
         );
       }
-      
-      this.ctxCover.fillStyle = colors[Math.floor(Math.random() * 40)];
-      this.ctxCover.fillRect(
-        Math.random() * innerWidth - 20,
-        Math.random() * innerHeight - 20,
-        Math.random() * innerWidth - 20,
-        Math.random() * innerHeight - 20
-      );
-      this.ctxCover.setTransform(1, 0, 0, .8, .2, 0);
-
-      requestAnimationFrame(this.loopCover);
+      ctx.setTransform(1, 0, .7, 1, .2, .4);
+      requestAnimationFrame(this.loopBack);
     }
   },
   mounted() {
     html2canvas(document.querySelector('#layout.layout-frame')).then(canvas => {
       this.subCanvas = canvas;
-      this.viewHeight = Math.round(this.$refs.viewCanvasBack.height / 100);
-      this.viewWidth = this.$refs.viewCanvasBack.width;
+      this.innerWidth = this.$refs.viewCanvas.width;
+      this.innerHeight = this.$refs.viewCanvas.height;
+      this.viewHeight = Math.round(this.$refs.viewCanvas.height / 100);
+      this.viewWidth = this.$refs.viewCanvas.width;
       this.subHeight = Math.round(this.subCanvas.height / 80);
       this.subwWidth = this.subCanvas.width;
-      this.ctxBack = this.$refs.viewCanvasBack.getContext("2d");
-      // this.ctxCover = this.$refs.viewCanvasCover.getContext("2d");
+      this.ctx = this.$refs.viewCanvas.getContext("2d");
       this.loopBack();
-      // this.loopCover();
       this.load = false;
       this.loadHandler();
     });
@@ -156,18 +105,11 @@ for (let i = 0; i < 3; i+= 1)  {
     width: 100%;
     height: 100vh;
     z-index: 50;
-    & > #viewCanvasBack {
+    & > #viewCanvas {
       position: fixed;
       width: 100%;
       height: 100%;
       z-index: 2;
-    }
-    & > #viewCanvasCover {
-      position: fixed;
-      width: 100%;
-      height: 100%;
-      z-index: 3;
-      opacity: .6;
     }
   }
 </style>
